7dc6d6834a5c8ffa8bf6abee63f2bdac88a04dcd
Wende Tan <twd2@163.com>
2019-04-18 00:09:05 +0800

    slower

--- slow_cat.py
+++ slow_cat.py
@@ -3,16 +3,21 @@
 import sys
 import time
 
+timing = [0.2, 0.02, 0.08]
+
+if len(sys.argv) >= 3:
+  timing = [0.5, 0.2, 0.8]
+
 try:
   with open(sys.argv[1], 'r') as f:
-    time.sleep(0.2)
+    time.sleep(timing[0])
     while True:
-      time.sleep(0.02)
+      time.sleep(timing[1])
       ch = f.read(1)
       if not ch:
         break
       if ch == '\n':
-        time.sleep(0.08)
+        time.sleep(timing[2])
       sys.stdout.write(ch)
       sys.stdout.flush()
   sys.stdout.close()
