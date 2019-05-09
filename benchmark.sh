e479811caf040b196701401385384fc2b622bbb4
Wende Tan <twd2@163.com>
2019-05-09 10:01:10 +0800

    update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-for allocator in allocator/simplemalloc/simplemalloc.so
+#allocator/simplemalloc/simplemalloc.so 
+for allocator in "" #allocator/dlmalloc-2.8.6/malloc.so allocator/tcmalloc-2.7/libtcmalloc.so allocator/jemalloc-5.2.0/libjemalloc.so allocator/uClibc-ng-1.0.31/ucmalloc.so allocator/avr-libc-2.0.0/malloc.so
 do
   for solver in random directed diversity
   do
     for ((i = 0; i < 10; i++))
     do
       echo ${allocator}-${solver}-${i}
-      ./solver.py -a "${allocator}" -s ${solver} -o results/${solver}_${i}_`date +%Y%m%d%H%M%S%N` naive test/naive
+      ./solver.py -a "${allocator}" -s ${solver} -o results/${solver}_${i}_`date +%Y%m%d%H%M%S%N` spec/naive.py test/naive
     done
   done
 done
