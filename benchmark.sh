29d05a55fb443c97559298c19f3bb19cd21c759f
Wende Tan <twd2@163.com>
2019-05-14 00:06:17 +0800

    update benchmark script to support multiply test executables

--- benchmark.sh
+++ benchmark.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #allocator/simplemalloc/simplemalloc.so 
-for allocator in "" #allocator/dlmalloc-2.8.6/malloc.so allocator/tcmalloc-2.7/libtcmalloc.so allocator/jemalloc-5.2.0/libjemalloc.so allocator/uClibc-ng-1.0.31/ucmalloc.so allocator/avr-libc-2.0.0/malloc.so allocator/musl-1.1.22/malloc.so
+for exe in test/bm_0noise_null test/bm_0noise_php-7.2.17 test/bm_0noise_python-3.6.7
 do
-  for solver in random directed diversity
+  for allocator in "" allocator/dlmalloc-2.8.6/malloc.so allocator/tcmalloc-2.7/libtcmalloc.so allocator/jemalloc-5.2.0/libjemalloc.so allocator/uClibc-ng-1.0.31/ucmalloc.so allocator/avr-libc-2.0.0/malloc.so allocator/musl-1.1.22/malloc.so
   do
-    for ((i = 0; i < 10; i++))
+    for solver in random directed #diversity
     do
-      echo ${allocator}-${solver}-${i}
-      ./solver.py -a "${allocator}" -s ${solver} -o results/${solver}_${i}_`date +%Y%m%d%H%M%S%N` spec/naive.py test/naive
+      for ((i = 0; i < 10; i++))
+      do
+        echo ${exe} ${allocator} ${solver} ${i}
+        ./solver.py -a "${allocator}" -s ${solver} -o results/benchmark_`date +%Y%m%d%H%M%S%N` spec/benchmark.py ${exe}
+      done
     done
   done
 done
